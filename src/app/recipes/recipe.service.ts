7182a9f2c77388c0f0416c6c0bb19ee04cea5fde
Tanima Ranjan <tanimaranjan@gmail.com>
2019-03-17 22:01:07 -0400

    Recipe Service updates

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,31 @@
 import { Recipe } from './recipe.model'
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>()
     
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'This is a test recipe', 'https://picsum.photos/200/300?image=1080'),
-        new Recipe('Test Recipe 2 ', 'This is a test recipe 2 ', 'https://picsum.photos/200/300?image=102')
+        new Recipe(
+            'Stawberry Pie ', 
+            'This is a test recipe', 
+            'https://picsum.photos/200/300?image=1080',
+            [
+                new Ingredient('Strawberry', 2),
+                new Ingredient('Sugar', 3),
+                new Ingredient('Flour', 1.5),
+                new Ingredient('Butter', 2)
+            ]),
+        new Recipe(
+            'Raspberry Chesscake ', 
+            'This is a test recipe 2 ', 
+            'https://picsum.photos/200/300?image=102',
+            [
+                new Ingredient('Raspberry', 2),
+                new Ingredient('Cream Cheese', 2),
+                new Ingredient('Sugar', 2),
+                new Ingredient('Milk', 2)
+            ])
        
      ];
    
